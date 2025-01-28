7ce79bdda928803a39ec083e1c9d5bbe2ddcc408
Ranjith-singh <singhranjith970@gmail.com>
2025-01-28 20:27:29 +0530

    created urlShortening project entities

---
+++ url-shortener-sb/src/main/java/com/url/shortener/models/UrlMapping.java
@@ -0,0 +1,32 @@
+package com.url.shortener.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import java.util.*;
+
+@Entity
+@Data
+public class UrlMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String originalUrl;
+    private String stortUrl;
+    private int clickCount = 0;
+    private LocalDateTime createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "urlMapping")
+    private List<ClickEvent> clickEvents;
+}
