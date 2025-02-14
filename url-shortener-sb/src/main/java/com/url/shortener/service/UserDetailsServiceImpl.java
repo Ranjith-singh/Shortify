abb07b1704abdb7903aefbcca0ffbf4b3fd6e70a
Ranjith-singh <singhranjith970@gmail.com>
2025-02-14 21:58:30 +0530

    user register and login

--- url-shortener-sb/src/main/java/com/url/shortener/service/UserDetailsServiceImpl.java
+++ url-shortener-sb/src/main/java/com/url/shortener/service/UserDetailsServiceImpl.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.url.shortener.models.User;
 import com.url.shortener.repository.UserRepository;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService{
 
     @Autowired
