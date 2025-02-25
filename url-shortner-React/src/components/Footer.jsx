fba67a38fd92e0f516b6c5af10c2cd0d026d93cf
Ranjith-singh <singhranjith970@gmail.com>
2025-02-26 01:01:41 +0530

    NavBar and Footer Added to the App.jsx

---
+++ url-shortner-React/src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { FaFacebook, FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
+import { SiGeeksforgeeks} from "react-icons/si"
+
+const Footer = () => {
+  return (
+    <footer className="bg-custom-gradient text-white py-8 z-40 relative">
+      <div className="container mx-auto px-6 lg:px-14 flex flex-col lg:flex-row lg:justify-between items-center gap-4">
+        <div className="text-center lg:text-left">
+          <h2 className="text-3xl font-bold mb-2">Linklytics</h2>
+          <p>Simplifying URL shortening for efficient sharing</p>
+        </div>
+
+        <p className="mt-4 lg:mt-0">
+          &copy; 2025 Linklytics. All rights reserved.
+        </p>
+
+        <div className="flex space-x-6 mt-4 lg:mt-0">
+          <a href="https://github.com/Ranjith-singh" className="hover:text-gray-200">
+            <FaGithub size={24} />
+          </a>
+          <a href="https://www.linkedin.com/in/ranjith-singh-s-061bba251/" className="hover:text-gray-200">
+            <FaLinkedin size={24} />
+          </a>
+          <a href="https://www.facebook.com/share/1ABuic9RY6/" className="hover:text-gray-200">
+            <FaFacebook size={24} />
+          </a>
+          <a href="https://www.geeksforgeeks.org/user/singhranpm5l/" className="hover:text-gray-200">
+            <SiGeeksforgeeks size={24} />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
