4c871e6b575f72bca8f0e0f3bfdd9c25b711852b
Ranjith-singh <singhranjith970@gmail.com>
2025-03-05 01:22:18 +0530

    add framer-motion to landing page

--- url-shortner-React/src/App.jsx
+++ url-shortner-React/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
 import AboutPage from './components/AboutPage'
 import NavBar from './components/NavBar'
@@ -8,14 +8,14 @@ import Footer from './components/Footer'
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <NavBar/>
         <Routes>
           <Route path='/' element={<LandingPage/>}/>
           <Route path='/about' element={<AboutPage/>}/>
         </Routes>
         <Footer/>
-      </BrowserRouter>
+      </Router>
     </>
   )
 }
