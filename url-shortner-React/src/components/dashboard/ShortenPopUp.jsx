7356782e8974e58520337b9440643388b29a152b
Ranjith-singh <singhranjith970@gmail.com>
2025-03-09 00:24:21 +0530

    Create Shorten url and copy it to the clipboard

--- url-shortner-React/src/components/dashboard/ShortenPopUp.jsx
+++ url-shortner-React/src/components/dashboard/ShortenPopUp.jsx
@@ -2,6 +2,7 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import React from "react";
+import CreateNewShorten from "./CreateNewShorten";
 
 const ShortenPopUp = ({ open, setOpen, refetch }) => { 
   const handleClose = () => {
@@ -15,14 +16,17 @@ const ShortenPopUp = ({ open, setOpen, refetch }) => {
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
-      <Box>
+        <div className='flex justify-center items-center h-full w-full'>
+            <CreateNewShorten setOpen={setOpen} refetch={refetch} />
+        </div>
+      {/* <Box>
         <Typography id="modal-modal-title" variant="h6" component="h2">
           Text in a modal
         </Typography>
         <Typography id="modal-modal-description" sx={{ mt: 2 }}>
           Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
         </Typography>
-      </Box>
+      </Box> */}
     </Modal>
   );
 };
